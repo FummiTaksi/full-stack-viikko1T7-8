ebcf3de5b6576f702d1bbfb901bf390c880b2309
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-01 19:36:12 +0200

    Sisalto komponentti valmis

--- src/index.js
+++ src/index.js
@@ -8,21 +8,37 @@ const Otsikko = (props) => {
         <h1>{props.text}</h1>
     )
 }
+
+const Sisalto = (props) => {
+    var elements = [];
+    for (var i = 0; i < props.courses.length; i++) {
+        var element = props.courses[i];
+        elements.push(<p> {element.name} {element.exercises}</p>);
+    }
+    return elements;
+}
+
+const lisaaOsaListaan = (lista, nimi, tehtavat) => {
+    lista.push({name: nimi, exercises: tehtavat});
+}
+
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
+  var elements = [];
   const osa1 = 'Reactin perusteet'
   const tehtavia1 = 10
+  lisaaOsaListaan(elements, osa1, tehtavia1);
   const osa2 = 'Tiedonvälitys propseilla'
   const tehtavia2 = 7
+  lisaaOsaListaan(elements, osa2 , tehtavia2);
   const osa3 = 'Komponenttien tila'
   const tehtavia3 = 14
+  lisaaOsaListaan(elements, osa3 , tehtavia3);
 
   return (
     <div>
       <Otsikko text = 'Half Stack -sovelluskehitys' />
-      <p>{osa1} {tehtavia1}</p>
-      <p>{osa2} {tehtavia2}</p>
-      <p>{osa3} {tehtavia3}</p>
+      <Sisalto courses = {elements} />
       <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
     </div>
   )
